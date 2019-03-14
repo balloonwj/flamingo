5e2378828b58bdb7ddc11bad23bc0d0ac83781e7
BitWait <imlvyong@163.com>
2019-03-14 14:33:16 +0800

    alter the endian

--- flamingoserver/net/InetAddress.cpp
+++ flamingoserver/net/InetAddress.cpp
@@ -36,7 +36,7 @@ InetAddress::InetAddress(uint16_t port, bool loopbackOnly)
 {
 	bzero(&addr_, sizeof addr_);
 	addr_.sin_family = AF_INET;
-	in_addr_t ip = loopbackOnly ? kInaddrLoopback : kInaddrAny;
+	in_addr_t ip = htobe32(loopbackOnly ? kInaddrLoopback : kInaddrAny);
 	addr_.sin_addr.s_addr = sockets::hostToNetwork32(ip);
 	addr_.sin_port = sockets::hostToNetwork16(port);
 }
